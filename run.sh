179d34e7ed3d332a26990b425f2853510905716d
Viktor Nagy <viktor.nagy@01people.com>
2021-08-27 16:09:14 +0200

    feat: bash version

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # SETTINGS
-BEEP_LENGTH=1
-LONG_BEEP_LENGTH=2
-
 EPOCHS=4
 CYCLES=30
 
@@ -12,20 +9,73 @@ HOLD_AFTER_INHALE=15
 
 
 play(){
+    # ffmpeg -f lavfi -i "sine=frequency=1027:duration=1" c.mp3
     case $1 in
+        c)
+            file=sounds/c6_1s.mp3
+            ;;
+        d)
+            file=sounds/d6_1s.mp3
+            ;;
+        e)
+            file=sounds/e6_1s.mp3
+            ;;
+        f)
+            file=sounds/f6_1s.mp3
+            ;;
+        g)
+            file=sounds/g6_1s.mp3
+            ;;
         a)
             file=sounds/a6_1s.mp3
             ;;
-        c)
-            file=sounds/c6_1s.mp3
+        h)
+            file=sounds/h6_1s.mp3
+            ;;
+        fast_c)
+            file=sounds/c6_05s.mp3
+            ;;
+        long_low_c)
+            file=sounds/c5_2s.mp3
+            ;;
+        meditation_ending)
+            file=sounds/meditation_ending.mp3
             ;;
     esac
     ffplay -nodisp -autoexit $file &> /dev/null
 }
 
-for (( i=1; i<=$EPOCHS; i++ ))
+for (( e=1; e<=$EPOCHS; e++ ))
 do
-    echo "Epoch: $i"
-    play c
+    echo "Epoch: $e"
+
+    # START SEQUENCE
+    play fast_c
+    play fast_c
+    play fast_c
+
+    # INTENSE BREATHING CYCLES
+    for (( c=1; c<=$CYCLES; c++ ))
+    do
+        echo "inhale"
+        play h
+        echo "exhale"
+        [ $c -eq $CYCLES ] \
+            && play e \
+            || play c
+    done
+
+    # LONG HOLD
+    if [[ $e -eq $EPOCHS ]]; then
+        play meditation_ending
+    else
+        echo "hold"
+        sleep $HOLD_AFTER_EXHALE
+
+        echo "inhale deeply"
+        play long_low_c
+        sleep $HOLD_AFTER_INHALE
+        play f
+    fi
 
 done
