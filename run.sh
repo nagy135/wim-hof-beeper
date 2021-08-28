4567eb3f82f19e6fafa9a35e63e7f63ecc68718f
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-08-28 12:16:29 +0200

    feat: flexibility in tone generation + change tones

--- run.sh
+++ run.sh
@@ -10,50 +10,108 @@ HOLD_AFTER_INHALE=5
 trap 'echo ...EXITING ; exit' SIGINT
 
 play(){
-    frequency=261
-    duration=1
     file=
+    duration=${2:-1}
     case $1 in
-        c)
-            frequency=261
-            duration=1
-            ;;
-        d)
-            frequency=293
-            duration=1
-            ;;
-        e)
-            frequency=329
-            duration=1
-            ;;
-        f)
-            frequency=349
-            duration=1
-            ;;
-        g)
-            frequency=391
-            duration=1
-            ;;
-        a)
-            frequency=440
-            duration=1
-            ;;
-        h)
-            frequency=493
-            duration=1
-            ;;
-        fast_c)
-            frequency=261
-            duration=0.5
-            ;;
-        long_low_c)
+        # 3 {{{
+        c3)
             frequency=130
-            duration=2
             ;;
+        d3)
+            frequency=146
+            ;;
+        e3)
+            frequency=164
+            ;;
+        f3)
+            frequency=174
+            ;;
+        g3)
+            frequency=195
+            ;;
+        a3)
+            frequency=220
+            ;;
+        h3 | b3)
+            frequency=246
+            ;;
+        # }}}
+        # 4 {{{
+        c | c4)
+            frequency=261
+            ;;
+        d | d4)
+            frequency=293
+            ;;
+        e | e4)
+            frequency=329
+            ;;
+        f | f4)
+            frequency=349
+            ;;
+        g | g4)
+            frequency=391
+            ;;
+        a | a4)
+            frequency=440
+            ;;
+        h | b | h4 | b4)
+            frequency=493
+            ;;
+        # }}}
+        # 5 {{{
+        c5)
+            frequency=523
+            ;;
+        d5)
+            frequency=587
+            ;;
+        e5)
+            frequency=659
+            ;;
+        f5)
+            frequency=698
+            ;;
+        g5)
+            frequency=783
+            ;;
+        a5)
+            frequency=880
+            ;;
+        h5 | b5)
+            frequency=987
+            ;;
+        # }}}
+        # 5 {{{
+        c6)
+            frequency=1046
+            ;;
+        d6)
+            frequency=1174
+            ;;
+        e6)
+            frequency=1318
+            ;;
+        f6)
+            frequency=1396
+            ;;
+        g6)
+            frequency=1567
+            ;;
+        a6)
+            frequency=1760
+            ;;
+        h6 | b6)
+            frequency=1975
+            ;;
+        # }}}
+        # sounds {{{
         meditation_ending)
             file=sounds/meditation_ending.mp3
             ;;
+        # }}}
     esac
+    echo "doing" $frequency $duration
     [ ! -z $file ] \
         && ffplay -nodisp -autoexit $file &> /dev/null \
         || ffplay -f lavfi -i "sine=frequency=$frequency:duration=$duration" -nodisp -autoexit &> /dev/null
@@ -65,33 +123,34 @@ while [ $e -le $EPOCHS ]; do
     e=$((e+1))
 
     # START SEQUENCE
-    play fast_c
-    play fast_c
-    play fast_c
+    play h5 0.5
+    play a5 0.5
+    play g5 0.5
 
     # INTENSE BREATHING CYCLES
     c=1
     while [ $c -le $CYCLES ]; do
         echo "inhale ($c)"
-        play h
+        play h3
         echo "exhale ($c)"
         [ $c -eq $CYCLES ] \
-            && play e \
-            || play c
+            && play a3 \
+            || play c3
         c=$((c+1))
     done
 
     # LONG HOLD
-    if [ $e -eq $EPOCHS ]; then
+    if [ $e -eq $((EPOCHS+1)) ]; then
         play meditation_ending
     else
         echo "hold"
         sleep $HOLD_AFTER_EXHALE
 
         echo "inhale deeply"
-        play long_low_c
+        play g3 2
         sleep $HOLD_AFTER_INHALE
-        play f
+        play f 0.5
+        play f 0.5
     fi
 
 done
